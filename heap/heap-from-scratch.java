7bca641b127d635222e18bacecbeb5d4b9f912fe
Jaymin Shah <shahjaymin2101@gmail.com>
2021-07-30 23:43:49 +0530

    Update heap-from-scratch.java

--- heap/heap-from-scratch.java
+++ heap/heap-from-scratch.java
@@ -2,61 +2,131 @@ class MinHeap
 {
     int[] arr;
     int capacity;
-    int hPtr;
+    int size;
     
     MinHeap(int capacity)
     {
-        this.capacity = capacity;
         arr = new int[capacity];
-        hPtr = 0;
+        this.capacity = capacity;
+        this.size = 0;
     }
+    
+    int left(int i) { return (i*2) + 1; }
+    int right(int i) { return (i*2) + 2; }
+    int parent(int i) { return (i-1) / 2; }
+    
+    void insert(int element)
+    {
+        if(size == capacity)
+        {
+            System.out.println("OVERFLOW!");
+            return;
+        }
+        size++;
+        arr[size - 1] = element;
+        int currIndex = size - 1;
+        while(currIndex != 0 && arr[parent(currIndex)] > arr[currIndex])
+        {
+            this.swap(parent(currIndex), currIndex);
+            currIndex = parent(currIndex);
+        }
+    }
+    
+    void heapify(int index)
+    {
+        int leftIndex = left(index);
+        int rightIndex = right(index);
+        int smallest = index;
+        
+        if(leftIndex < size && arr[leftIndex] < arr[smallest]) { smallest = leftIndex; }
+        if(rightIndex < size && arr[rightIndex] < arr[smallest]) { smallest = rightIndex; }
+        
+        if(smallest != index) 
+        {
+            swap(smallest, index);
+            heapify(smallest);
+        }
+    }
+    
+    int extractMin()
+    {
+        if(size == 0)
+        {
+            return -1;
+        }
+        size--;
+        int min = arr[0];
+        if(size == 0)
+        {
+            return min;
+        }
+        arr[0] = arr[size];
+        heapify(0);
+        return min;
+    }
+    
+    void decreaseKey(int key, int value)
+    {
+        if(key > size || value > arr[key])
+        {
+            System.out.println("Invalid operation!");
+            return;
+        }
+        arr[key] = value;
+        while(key != 0 && arr[parent(key)] > arr[key])
+        {
+            swap(parent(key), key);
+            key = parent(key);
+        }
+    }
+    
+    void delete(int index)
+    {
+        if(index > size)
+        {
+            System.out.print("Invalid operation!");
+            return;
+        }
+        size--;
+        if(size != 0)
+        {
+            arr[index] = arr[size];
+            heapify(index);
+        }
+    }
+    
+    void printHeap()
+    {
+        for(int i = 0; i < size; i++)
+        {
+            System.out.print(arr[i] + "  ");
+        }
+        System.out.println();
+    }
+    
     void swap(int a, int b)
     {
         int temp = arr[a];
         arr[a] = arr[b];
         arr[b] = temp;
     }
-    int parent(int i) { return arr[(i-1)*2]; }
-    int left(int i) { return arr[(i*2) + 1]; }
-    int right(int i) { return arr[(i*2) + 2]; }
-    void insert(int data)
-    {
-        if(hPtr == capacity)
-        {
-            System.out.println("OVERFLOW!\n");
-            return;
-        }
-        
-        hPtr++;
-        int index = hPtr - 1;
-        arr[index] = data;
-        
-        while(index > 0 && arr[parent(index)] > arr[index])
-        {
-            swap(parent(index), index);
-            index = parent(index);
-        }
-        
-    }
-    int getMin()
-    {
-        // if(hPtr == 0)
-        // {
-        //     return -1;
-        // }
-        return arr[hPtr];
-    }
 }
-
 
 public class Main
 {
 	public static void main(String[] args) 
 	{
-		MinHeap heap = new MinHeap(20);
+		MinHeap heap = new MinHeap(10);
 		heap.insert(10);
-// 		heap.insert(4);
-// 		heap.insert(1);
-		System.out.println(heap.getMin());
+		heap.insert(50);
+		heap.insert(100);
+		heap.insert(1);
+		heap.insert(7458);
+		heap.insert(52);
+		heap.insert(66);
+		heap.insert(7);
+		heap.printHeap();
+		heap.delete(4);
+		heap.printHeap();
 	}
 }
